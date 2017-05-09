1c51a9bc9e5e44b15aeee2879d1f4e9a4d0b5d58
torrencem <matt@torrencefamily.net>
2017-05-09 11:58:30 -0400

    Updated blender plugin

--- kblend.py
+++ kblend.py
@@ -1,79 +1,95 @@
-# This example assumes we have a mesh object selected
-
 import bpy
-import bmesh
+from bpy.props import *
 from math import sin, cos, sqrt, pi, atan2
 
-# Get the active mesh
-me = bpy.context.object.data
 
+def uObj(ob):
+    if not ob.doorbit:
+        return
+    bigG = 6.67408e-11
+    mEarth = 5.972e24
+    rmax = ob.rmax
+    rmin = ob.rmin
+    time = ob.time
+    ecc = (rmax - rmin) / (rmax + rmin)
 
-# Get a BMesh representation
-bm = bmesh.new()   # create an empty BMesh
-bm.from_mesh(me)   # fill it in from a Mesh
+    p = 2 / (1 / rmin + 1 / rmax)
 
+    a = p / (1 - ecc ** 2)
+    b = p / sqrt(1 - ecc ** 2)
 
-#v1 = bm.verts.new((2.0, 2.0, 2.0))
-#v2 = bm.verts.new((-2.0, 2.0, 2.0))
-#v3 = bm.verts.new((-2.0, -2.0, 2.0))
+    period = 2 * pi * sqrt(a ** 3 / (bigG * mEarth))
 
-#bm.faces.new((v1, v2, v3))
-
-
-rmin = 2.783e6
-rmax = 9.253e6
-bigG = 6.67408e-11
-mEarth = 5.972e24
-ecc = (rmax - rmin) / (rmax + rmin)
-
-p = 2 / (1 / rmin + 1 / rmax)
-
-a = p / (1 - ecc ** 2)
-b = p / sqrt(1 - ecc ** 2)
-
-period = 2 * pi * sqrt(a ** 3 / (bigG * mEarth))
-
-print(period)
-
-def calculateCoords(time):
     meananomaly = (time % period) * (2 * pi) / period
 
-    # Find eccentric anomaly
     eanom = meananomaly
-
-    # Use Newton's Method to approximate
-    # the eccentric anomaly
     for it in range(9):
         eanom = meananomaly + ecc * sin(eanom)
 
-    # Now solve for the true anomaly theta
     theta = 2 * atan2(sqrt(1 + ecc) * sin(eanom / 2), sqrt(1 - ecc) * cos(eanom / 2))
 
     radius = p / (1 + ecc * cos(theta))
 
-    return (radius, theta)
+    radius /= 4e5  # Make it smaller
 
-def drawRadial(r, theta):
-    xypos = [r * cos(theta), r * sin(theta)]
-    if preVert is None:
-        preVert = bm.verts.new((xypos[0], xypos[1], 0))
-    else:
-        newVert = bm.verts.new((xypos[0], xypos[1], 0))
-        bm.edges.new((preVert, newVert))
-        preVert = newVert
+    xco, yco = radius * cos(theta), radius * sin(theta)
+
+    ob.location = (xco, yco, 0)
 
 
-preVert = None
+def updateProperties(self, context):
+    ob = context.object
+    if not ob:
+        return
 
-for i in range(78):
-    seconds = i * 60
-    position = list(calculateCoords(seconds))
-    position[0] /= 4e4
-    print(position)
-    drawRadial(*position)
-
+    # Update the position of the object
+    uObj(ob)
 
 
-# Finish up, write the bmesh back to the mesh
-bm.to_mesh(me)
-bm.free()  # free and prevent further access
+bpy.types.Object.doorbit = BoolProperty(
+    name="Do Orbit",
+    default=False,
+    update=updateProperties)
+
+bpy.types.Object.rmin = FloatProperty(
+    name="Min Radius",
+    default=2.783e6,
+    update=updateProperties)
+
+bpy.types.Object.rmax = FloatProperty(
+    name="Max Radius",
+    default=6.783e6,
+    update=updateProperties)
+
+bpy.types.Object.time = FloatProperty(
+    name="Simulation Time",
+    default=0,
+    update=updateProperties)
+
+
+class OrbitPanel(bpy.types.Panel):
+    bl_label = "Orbit Properties"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+
+    def draw(self, context):
+        ob = context.object
+        if not ob:
+            return
+        layout = self.layout
+        layout.prop(ob, 'doorbit')
+        layout.prop(ob, 'rmin')
+        layout.prop(ob, 'rmax')
+        layout.prop(ob, 'time')
+
+
+# Registration
+bpy.utils.register_class(OrbitPanel)
+
+
+def my_handler(scene):
+    for obj in scene.objects:
+        uObj(obj)
+
+
+bpy.app.handlers.frame_change_post.append(my_handler)
